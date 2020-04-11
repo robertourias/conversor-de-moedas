ab18dd5588bc5bf68df51be58a1a1a2425edcabd
Roberto Nicoletti <roberto.urias@gmail.com>
2020-04-10 22:35:39 -0300

    Service moeda

--- src/app/conversor/conversor.module.ts
+++ src/app/conversor/conversor.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ConversorComponent } from './components';
+import { MoedaService } from './services';
 
 @NgModule({
-  declarations: [ConversorComponent],
+  declarations: [
+    ConversorComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    ConversorComponent
+  ],
+  providers: [
+    MoedaService
   ]
 })
 export class ConversorModule { }
