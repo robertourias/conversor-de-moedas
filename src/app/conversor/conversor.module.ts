6f8890503ac699e31e421ef40890f3cab5136600
Roberto Nicoletti <roberto.urias@gmail.com>
2020-04-10 23:04:48 -0300

    Implementando serviço do conversor de moedas

--- src/app/conversor/conversor.module.ts
+++ src/app/conversor/conversor.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
 import { ConversorComponent } from './components';
 import { MoedaService, ConversorService } from './services';
 
 @NgModule({
   declarations: [
-    ConversorComponent
+    ConversorComponent,
+    HttpClientModule
   ],
   imports: [
-    CommonModule
+    CommonModule,
+
   ],
   exports: [
     ConversorComponent
